605be7d7dbb9668f8f4353c3558d13cde0e87ad5
KoraReinolds <s.shurin94@gmail.com>
2021-10-09 00:17:12 +0300

    initial chart

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,49 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <LineChart
+    :chartData="chartData"
+    :chartOptions="{
+      responsive: false,
+    }"
+  />
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+import { defineComponent } from 'vue';
+import LineChart from '@/components/LineChart';
 
-export default {
-  name: 'Home',
+export default defineComponent({
+  name: 'App',
   components: {
-    HelloWorld,
+    LineChart,
   },
-};
+  setup() {
+    return {
+      chartData: {
+        labels: ['ср', 'чт', 'пт', 'сб', 'вс', 'пн', 'вт'],
+        datasets: [
+          {
+            label: 'белки',
+            borderColor: '#BBBDB9',
+            backgroundColor: 'transparent',
+            data: [40, 20, 12, 39, 10, 40, 39],
+          },
+          {
+            label: 'жиры',
+            borderColor: '#FADA05',
+            backgroundColor: 'transparent',
+            data: [50, 30, 52, 19, 20, 40, 39],
+          },
+          {
+            label: 'углеводы',
+            borderColor: '#7A5A62',
+            backgroundColor: 'transparent',
+            data: [20, 30, 52, 59, 40, 10, 39],
+          },
+        ],
+      },
+    };
+  },
+});
 </script>
+<style lang="stylus">
+</style>
