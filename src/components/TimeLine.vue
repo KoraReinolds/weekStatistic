c86dd4f868763123f1d09e709c72f3c3bcfe786b
KoraReinolds <s.shurin94@gmail.com>
2021-10-12 00:22:53 +0300

    add timeline

---
+++ src/components/TimeLine.vue
@@ -0,0 +1,59 @@
+<template>
+  <div
+    class="timeline"
+  >
+    <span
+      class="section"
+      v-for="(section, index) in timeLineData"
+      :key="`section-${index}`"
+    >
+      <span
+        class="date"
+      >
+        {{ section.data }}
+      </span>
+      <span
+        class="food"
+        v-for="(food, index) in section.food"
+        :key="`food-${index}`"
+      >
+        {{ food.name }}
+      </span>
+    </span>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Product',
+  props: {
+    timeLineData: {
+      type: Array,
+    },
+  },
+  setup() {
+  },
+});
+</script>
+<style lang="stylus">
+.timeline
+  margin: 30px;
+  display: flex
+  flex-direction: row
+  justify-content: space-between
+  align-items: center
+
+  .section
+    display: flex
+    flex-direction: column
+    padding: 0 auto
+    width: 100%
+
+    &:not(:last-child)
+      border-right: 1px solid black
+
+    .date
+      padding: 20px;
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,10 +6,10 @@
     }"
   />
   <product
-    :product="{
-      name: 'каша',
-      bju: [70, 20, 10],
-    }"
+    :product="product"
+  />
+  <time-line
+    :timeLineData="timeLineData"
   />
 
 </template>
@@ -18,17 +18,34 @@
 import { defineComponent } from 'vue';
 import LineChart from '@/components/LineChart';
 import Product from '@/components/Product.vue';
+import TimeLine from '@/components/TimeLine.vue';
 
 export default defineComponent({
   name: 'App',
   components: {
     LineChart,
     Product,
+    TimeLine,
   },
   setup() {
+    const product = {
+      name: 'каша',
+      bju: [70, 20, 10],
+    };
+    const days = [...Array(7).keys()].map((i) => {
+      const today = +new Date() + i * 24 * 3600 * 1000;
+      return new Date(today);
+    });
+    const labels = days.map((d) => d.toISOString().split('T')[0].split('-').slice(1, 3).join('/'));
+    const timeLineData = labels.map((data) => ({
+      data,
+      food: [product],
+    }));
     return {
+      product,
+      timeLineData,
       chartData: {
-        labels: ['ср', 'чт', 'пт', 'сб', 'вс', 'пн', 'вт'],
+        labels,
         datasets: [
           {
             label: 'белки',
